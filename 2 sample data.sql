86f8dba1ce3bd532ef04466dff68b54b26db1497
EstyWalter <estywalter4@gmail.com>
2025-12-28 19:52:32 -0500

    added-sample-data

---
+++ 2 sample data.sql
@@ -0,0 +1,8 @@
+insert body(bodypart, num)
+select 'eyes', 2
+union select 'ears', 2
+union select 'nose', 1
+union select 'mouth', 2
+union select 'teeth', 32
+insert body(bodypart, num)
+select 'toungue', 1
